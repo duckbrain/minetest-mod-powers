35fbac71bd5f01965f71b0941f5b6aab4a277fe5
Jonathan Duck <duckbrain30@gmail.com>
2016-06-12 09:15:52 -0700

    State store between steps and removing powers on set

--- power_all.lua
+++ power_all.lua
@@ -3,29 +3,29 @@ powers.register_power({
   name = "all",
   exclude = true,
 
-  on_gain = function(player)
+  on_gain = function(player, state)
     for i = 1,table.getn(powers),1 do
       local power = powers[i]
       if not power.exclude and power.on_gain then
-			  power.on_gain(player)
+			  power.on_gain(player, state)
       end
 		end
   end,
 
-  on_loose = function(player)
+  on_loose = function(player, state)
     for i = 1,table.getn(powers),1 do
       local power = powers[i]
       if not power.exclude and power.on_loose then
-			  power.on_loose(player)
+			  power.on_loose(player, state)
       end
 		end
   end,
 
-  on_step = function(player, toggle)
+  on_step = function(player, state)
     for i = 1,table.getn(powers),1 do
       local power = powers[i]
       if not power.exclude and power.on_step then
-        power.on_step(player, toggle)
+        power.on_step(player, state)
       end
     end
   end,
