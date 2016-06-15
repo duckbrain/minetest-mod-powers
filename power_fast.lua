5452565330bf88208bfb8fa016a4ef809950e932
Jonathan Duck <duckbrain30@gmail.com>
2016-06-15 08:45:50 -0700

    Fast only works on land

--- power_fast.lua
+++ power_fast.lua
@@ -1,3 +1,10 @@
+function get_nodedef_field(nodename, fieldname)
+  if not minetest.registered_nodes[nodename] then
+    return nil
+  end
+  return minetest.registered_nodes[nodename][fieldname]
+end
+
 powers.register_power({
 
   name = "fast",
@@ -6,7 +13,15 @@ powers.register_power({
     local controls = player:get_player_control()
     -- TODO: Only allow fast if you were last standing on land
     if controls.aux1 then
-      player:set_physics_override({speed = 2.0})
+  		local pos = player:getpos()
+  		pos.y = math.floor(pos.y)
+      local onnode = get_nodedef_field(minetest.get_node(pos).name, "drawtype")
+      pos.y = pos.y + 1
+      local innode = get_nodedef_field(minetest.get_node(pos).name, "drawtype")
+
+      if innode == "liquid" then player:set_physics_override({speed = 1.0})
+      elseif onnode == "normal" then player:set_physics_override({speed = 2.0})
+      end
     else
       player:set_physics_override({speed = 1.0})
     end
